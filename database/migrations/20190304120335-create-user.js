40d9822d019c3afb2417e1350b78a753782ed2de
Moiseienko Mykola <mmoiseienko@gfl.geeksforless.net>
2019-03-04 14:39:11 +0200

    User model and migration add;

---
+++ database/migrations/20190304120335-create-user.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            name: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            email: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            password: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            role: {
+                allowNull: false,
+                type: Sequelize.ENUM('admin', 'user')
+            },
+            discount: {
+                allowNull: false,
+                type: Sequelize.DECIMAL(8, 2)
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('Users');
+    }
+};
